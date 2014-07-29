16cfcb49d6ceccb0a5d91139cb62ff36c90c2848
Brandon Kelly <brandon@pixelandtonic.com>
2014-07-29 11:14:05 -0700

    Initial commit

---
+++ craft/config/general.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
+ */
+
+return array(
+
+);
