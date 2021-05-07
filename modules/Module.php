e2d3ae179fe6e4e0be524e3d5ef3669ad10084df
Benoît Rouleau <benoit.rouleau@icloud.com>
2021-05-07 02:04:12 -0400

    Update Craft CMS, plugins, and config + add Freeform

--- modules/Module.php
+++ modules/Module.php
@@ -27,7 +27,16 @@ class Module extends \yii\base\Module
      */
     public function init()
     {
+        // Set a @modules alias pointed to the modules/ directory
         Craft::setAlias('@modules', __DIR__);
+
+        // Set the controllerNamespace based on whether this is a console or web request
+        if (Craft::$app->getRequest()->getIsConsoleRequest()) {
+            $this->controllerNamespace = 'modules\\console\\controllers';
+        } else {
+            $this->controllerNamespace = 'modules\\controllers';
+        }
+
         parent::init();
 
         // Custom initialization code goes here...
--- web/index.php
+++ web/index.php
@@ -3,19 +3,24 @@
  * Craft web bootstrap file
  */
 
-// Set path constants
+// Define path constants
 define('CRAFT_BASE_PATH', dirname(__DIR__));
-define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH.'/vendor');
+define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH . '/vendor');
 
 // Load Composer's autoloader
-require_once CRAFT_VENDOR_PATH.'/autoload.php';
+require_once CRAFT_VENDOR_PATH . '/autoload.php';
 
 // Load dotenv?
-if (file_exists(CRAFT_BASE_PATH.'/.env')) {
-    (new Dotenv\Dotenv(CRAFT_BASE_PATH))->load();
+if (class_exists('Dotenv\Dotenv') && file_exists(CRAFT_BASE_PATH . '/.env')) {
+    Dotenv\Dotenv::create(CRAFT_BASE_PATH)->load();
 }
 
-// Load and run Craft
+// Define additional PHP constants
+// (see https://craftcms.com/docs/3.x/config/#php-constants)
 define('CRAFT_ENVIRONMENT', getenv('ENVIRONMENT') ?: 'production');
-$app = require CRAFT_VENDOR_PATH.'/craftcms/cms/bootstrap/web.php';
+// ...
+
+// Load and run Craft
+/** @var craft\web\Application $app */
+$app = require CRAFT_VENDOR_PATH . '/craftcms/cms/bootstrap/web.php';
 $app->run();
