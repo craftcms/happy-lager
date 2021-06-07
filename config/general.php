5eef7b1accfddbfe8523d472874991237c530ef4
brandonkelly <brandon@pixelandtonic.com>
2021-06-07 15:26:50 -0700

    Modernize config/general.php

--- config/general.php
+++ config/general.php
@@ -8,46 +8,35 @@
  * @see \craft\config\GeneralConfig
  */
 
+use craft\helpers\App;
+
+$isDev = App::env('ENVIRONMENT') === 'dev';
+$isProd = App::env('ENVIRONMENT') === 'production';
+
 return [
-    // Global settings
-    '*' => [
-        // Default Week Start Day (0 = Sunday, 1 = Monday...)
-        'defaultWeekStartDay' => 1,
+    // Default Week Start Day (0 = Sunday, 1 = Monday...)
+    'defaultWeekStartDay' => 1,
 
-        // Whether generated URLs should omit "index.php"
-        'omitScriptNameInUrls' => true,
+    // Whether generated URLs should omit "index.php"
+    'omitScriptNameInUrls' => true,
 
-        // Control Panel trigger word
-        'cpTrigger' => 'admin',
+    // The URI segment that tells Craft to load the control panel
+    'cpTrigger' => App::env('CP_TRIGGER') ?: 'admin',
 
-        // The secure key Craft will use for hashing and encrypting data
-        'securityKey' => getenv('SECURITY_KEY'),
+    // The secure key Craft will use for hashing and encrypting data
+    'securityKey' => App::env('SECURITY_KEY'),
 
-        // Whether to save the project config out to config/project.yaml
-        // (see https://docs.craftcms.com/v3/project-config.html)
-        'useProjectConfigFile' => false,
+    // Whether Dev Mode should be enabled (see https://craftcms.com/guides/what-dev-mode-does)
+    'devMode' => $isDev,
 
-        'aliases' => [
-            '@assetBasePath' => getenv('ASSET_BASE_PATH'),
-            '@assetBaseUrl' => getenv('ASSET_BASE_URL'),
-        ],
-    ],
+    // Whether administrative changes should be allowed
+    'allowAdminChanges' => true,
 
-    // Dev environment settings
-    'dev' => [
-        // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
-        'devMode' => true,
-    ],
+    // Whether crawlers should be allowed to index pages and following links
+    'disallowRobots' => !$isProd,
 
-    // Staging environment settings
-    'staging' => [
-        // Set this to `false` to prevent administrative changes from being made on staging
-        'allowAdminChanges' => true,
-    ],
-
-    // Production environment settings
-    'production' => [
-        // Set this to `false` to prevent administrative changes from being made on production
-        'allowAdminChanges' => true,
+    'aliases' => [
+        '@assetBasePath' => getenv('ASSET_BASE_PATH'),
+        '@assetBaseUrl' => getenv('ASSET_BASE_URL'),
     ],
 ];
