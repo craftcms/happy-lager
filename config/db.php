4a950570cd9925350b4b0ee21319cdcaf1c0b714
brandonkelly <brandon@pixelandtonic.com>
2021-06-07 15:23:39 -0700

    Modernize config/db.php

--- config/db.php
+++ config/db.php
@@ -4,15 +4,22 @@
  *
  * All of your system's database connection settings go in here. You can see a
  * list of the available settings in vendor/craftcms/cms/src/config/DbConfig.php.
+ *
+ * @see craft\config\DbConfig
  */
 
+use craft\helpers\App;
+
 return [
-    'driver' => getenv('DB_DRIVER'),
-    'server' => getenv('DB_SERVER'),
-    'user' => getenv('DB_USER'),
-    'password' => getenv('DB_PASSWORD'),
-    'database' => getenv('DB_DATABASE'),
-    'schema' => getenv('DB_SCHEMA'),
-    'tablePrefix' => getenv('DB_TABLE_PREFIX'),
-    'port' => getenv('DB_PORT')
+    'dsn' => App::env('DB_DSN') ?: null,
+    'driver' => App::env('DB_DRIVER'),
+    'server' => App::env('DB_SERVER'),
+    'port' => App::env('DB_PORT'),
+    'database' => App::env('DB_DATABASE'),
+    'user' => App::env('DB_USER'),
+    'password' => App::env('DB_PASSWORD'),
+    'schema' => App::env('DB_SCHEMA'),
+    'tablePrefix' => App::env('DB_TABLE_PREFIX'),
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
 ];
